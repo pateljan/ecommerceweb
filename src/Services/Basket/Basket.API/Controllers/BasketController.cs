4bd11bee5bfd096d705b140efd1b4292bcbf6368
Janak Patel <pateljan@gmail.com>
2021-12-06 09:42:24 +0530

    Basket.Api, redis db and portainer implementation.

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,50 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(IBasketRepository basketRepository, ILogger<BasketController> logger)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet("{username}", Name = "GetBaskets")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBaskets(string username)
+        {
+            var basket = await _basketRepository.GetBasket(username);
+            return Ok(basket ?? new ShoppingCart(username));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _basketRepository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{username}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            await _basketRepository.DeleteBasket(username);
+            return Ok();
+        }
+
+    }
+}
